3a9b84d99a8c423c4ab552b7cfa0c4d3ee63d070
HilalLahreche <hilallahreche@gmail.com>
2024-05-06 16:04:46 +0200

    horloge digitale et jour de la semaine ok

--- clock.js
+++ clock.js
@@ -1,14 +1,27 @@
-function updateClock() {
-    var now = new Date();
-    var hours = now.getHours().toString().padStart(2, '0');
-    var minutes = now.getMinutes().toString().padStart(2, '0');
-    var seconds = now.getSeconds().toString().padStart(2, '0');
-    var timeString = hours + ':' + minutes + ':' + seconds;
-    document.getElementById('clock').textContent = timeString;
+function currentTime() {
+    var date = new Date(); 
+    var dayOfWeek = date.getDay(); // Obtient le jour de la semaine (0 pour dimanche, 1 pour lundi, etc.)
+    var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    var day = days[dayOfWeek];
+
+    var dayOfMonth = date.getDate(); // Obtient le jour du mois (1 à 31)
+    var month = date.toLocaleString('fr-FR', { month: 'long' }); // Obtient le mois au format long (par exemple, "mai")
+
+    var hour = date.getHours(); 
+    var min = date.getMinutes(); 
+    var sec = date.getSeconds(); 
+
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+
+    document.getElementById("clock").innerText = day + " " + dayOfMonth + " " + month + ", " + hour + " : " + min + " : " + sec;
+    
+    setTimeout(currentTime, 1000); 
 }
 
-// Mettre à jour l'heure toutes les secondes
-setInterval(updateClock, 1000);
+function updateTime(k) {
+    return (k < 10) ? "0" + k : k;
+}
 
-
-updateClock();
+currentTime(); 
