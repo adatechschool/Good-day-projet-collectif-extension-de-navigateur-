bc756be21be6f2ca19fd0e32d8ab3bf31228830d
HilalLahreche <hilallahreche@gmail.com>
2024-05-06 20:22:25 +0200

    modidcation image cible del'Api  ciblage image morning

--- background.js
+++ background.js
@@ -3,7 +3,8 @@
 function getRandomImage() {
     // Remplacez "YOUR_ACCESS_KEY" par votre clé d'accès à l'API Unsplash
     var accessKey = 'Zn7WSdPyRExR6bWblabkMgVThpGRk57Jn9THLis9Hvg';
-    var url = 'https://api.unsplash.com/photos/random?client_id=' + accessKey;
+    
+    var url = 'https://api.unsplash.com/photos/random?query=morning&client_id=' + accessKey;
 
     fetch(url)
     .then(response => response.json())
@@ -21,4 +22,4 @@ function setBackgroundImage(url) {
 }
 
 
-getRandomImage();
+//getRandomImage();
