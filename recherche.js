ca1dc54b2e3746cfe61c5394cf68bd0f843d134e
HilalLahreche <hilallahreche@gmail.com>
2024-05-06 14:30:39 +0200

    version MVP besoins d'affinement CSS todolist

--- recherche.js
+++ recherche.js
@@ -15,3 +15,30 @@ document.getElementById( "wikipediaSearch" ).addEventListener("click", function 
         chrome.tabs.create( { url: wikipediaSearchUrl});
     }
 });
+
+document.getElementById("qwantSearch").addEventListener("click", function (){
+    const query = document.getElementById("SearchBox").value;
+    if (query) {
+        const qwantSearchUrl = `https://www.qwant.com/?q=${encodeURIComponent(query)}`;
+        chrome.tabs.create({ url: qwantSearchUrl });
+    }
+});
+document.getElementById("ecosiaSearch").addEventListener("click", function (){
+    const query = document.getElementById("SearchBox").value;
+    if (query) {
+        const ecosiaSearchUrl = `https://www.ecosia.org/search?q=${encodeURIComponent(query)}`;
+        chrome.tabs.create({ url: ecosiaSearchUrl });
+    }
+});
+
+document.getElementById("youcareSearch").addEventListener("click", function (){
+    const query = document.getElementById("SearchBox").value;
+    if (query) {
+        const youcareSearchUrl = `https://www.youcare.world/all?q=${encodeURIComponent(query)}`;
+        chrome.tabs.create({ url: youcareSearchUrl });
+    }
+});
+
+
+
+
