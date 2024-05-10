9c1374b0297db25462004fc4961ae7bb431a727a
HilalLahreche <hilallahreche@gmail.com>
2024-05-10 18:43:43 +0200

    version responsive  a presenter

---
+++ animate.js
@@ -0,0 +1,9 @@
+// function animate() {
+//     document.getElementById("wikipediaSearch").classList.remove('btn');
+//     // document.getElementById("googleSearch").classList.remove('btn');
+//     // document.getElementById("qwantSearch").classList.remove('btn');
+//     // document.getElementById("ecosiaSearch").classList.remove('btn');
+//     // document.getElementById("youcareSearch").classList.remove('btn');
+// }
+
+// animate();
